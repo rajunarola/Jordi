25daa0977d2373d615763b871efa4fc05951f0fb
rajunarola <rkg@narola.email>
2021-03-19 10:26:05 +0530

    hgjgj

---
+++ SocialEcommerce.Shared/Constants/EmailConstant.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialEcommerce.Shared.Constants
+{
+    public static class EmailConstant
+    { 
+
+        public const string  MailFrom = "narolamma@gmail.com";
+        public const string SmtpHost = "smtp.gmail.com";
+        public const int SmtpPort = 587;
+        public const bool UseDefaultCredentials = true;
+        public const string EmailPassword = "Password123#";
+        public const bool EnableSsl = true;
+
+        public static class TemplateName
+        {
+            public const string ConfirmEmail = @"ConfirmEmailTemplate.html";
+            public const string ForgotPassword = @"ForgetPasswordTemplate.html";
+
+
+        }
+    }
+
+   
+}
---
+++ SocialEcommerce.Shared/Utilities/Utility.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SocialEcommerce.Shared.Utilities
+{
+    public static class Utility
+    {  
+        public static string GetEmailTemplate(string directory, string fileName)
+        {
+           // string path = "~/";
+            string header = "", footer = "", body = "";
+
+            using (StreamReader reader = new StreamReader("~/EmailTemplates/_layoutTemplate/EmailHeader.html"))
+            {
+                header = reader.ReadToEnd();
+            }
+            using (StreamReader reader = new StreamReader("~/EmailTemplates/" + directory   + "/" + fileName))
+            {
+                body = reader.ReadToEnd();
+            }
+            using (StreamReader reader = new StreamReader("~/EmailTemplates/_layoutTemplate/EmailFooter.html" ))
+            {
+                footer = reader.ReadToEnd();
+            }
+            return header + body + footer;
+        }
+
+        public static string ReadEmailTemplate(string currentPath, string templateName, string physicalUrl)
+        {
+            try
+            {
+                string fullPath = CheckServerPath(currentPath, @"EmailTemplates\", templateName);
+                string body;
+                using (var sr = new StreamReader(fullPath))
+                {
+                    body = sr.ReadToEnd();
+                }
+
+                body = body.Replace("{url}", physicalUrl);
+                return body;
+            }
+            catch (Exception ex)
+            {
+               
+                return ex.Message;
+            }
+        }
+
+        public static string CheckServerPath(string currentPath, string folderPath, string fileName)
+        {
+            var fullPath = Path.Combine(currentPath, folderPath);
+            if (!Directory.Exists(fullPath))
+                Directory.CreateDirectory(fullPath);
+            return Path.Combine(fullPath, fileName);
+        }
+
+    }
+}
